99bf076b12cd016d9b37150a44fb2dd18bd9b73f
josh wheeler <joshdub223@gmail.com>
2014-11-19 15:14:09 -0500

    Add loading script for tpch sf10

---
+++ hdfs/schema/load.sh
@@ -0,0 +1,8 @@
+hdfs dfs -put /damsl/projects/datasets/tpch/sf10/singlefile/customer.out hdfs://qp-hm1.damsl.cs.jhu.edu:54310/tpch/10g/customer/customer
+hdfs dfs -put /damsl/projects/datasets/tpch/sf10/singlefile/lineitem.out hdfs://qp-hm1.damsl.cs.jhu.edu:54310/tpch/10g/lineitem/lineitem
+hdfs dfs -put /damsl/projects/datasets/tpch/sf10/singlefile/nation.out hdfs://qp-hm1.damsl.cs.jhu.edu:54310/tpch/10g/nation/nation
+hdfs dfs -put /damsl/projects/datasets/tpch/sf10/singlefile/orders.out hdfs://qp-hm1.damsl.cs.jhu.edu:54310/tpch/10g/orders/orders
+hdfs dfs -put /damsl/projects/datasets/tpch/sf10/singlefile/part.out hdfs://qp-hm1.damsl.cs.jhu.edu:54310/tpch/10g/part/part
+hdfs dfs -put /damsl/projects/datasets/tpch/sf10/singlefile/partsupp.out hdfs://qp-hm1.damsl.cs.jhu.edu:54310/tpch/10g/partsupp/partsupp
+hdfs dfs -put /damsl/projects/datasets/tpch/sf10/singlefile/region.out hdfs://qp-hm1.damsl.cs.jhu.edu:54310/tpch/10g/region/region
+hdfs dfs -put /damsl/projects/datasets/tpch/sf10/singlefile/supplier.out hdfs://qp-hm1.damsl.cs.jhu.edu:54310/tpch/10g/supplier/supplier
