2beba5a381ac3ff7f7ee18c7ad1cec32026282a8
Yanif Ahmad <yanif@jhu.edu>
2014-11-19 21:12:59 -0500

    Added vertica schema, and cleaned up oracle schema.

--- workloads/tpch/vertica/sql/schema.sql
+++ workloads/tpch/vertica/sql/schema.sql
@@ -7,10 +7,9 @@ create table part (
    p_size        integer       not null,
    p_container   char(10)      not null,
    p_retailprice decimal(12,2) not null,
-   p_comment     varchar(23)   not null,
-   constraint part_pk primary key (p_partkey)
+   p_comment     varchar(23)   not null
 )
-NOLOGGING INMEMORY MEMCOMPRESS FOR QUERY;
+constraint part_pk primary key (p_partkey);
 
 create table supplier (
    s_suppkey   integer       not null,
@@ -19,20 +18,18 @@ create table supplier (
    s_nationkey integer       not null,
    s_phone     char(15)      not null,
    s_acctbal   decimal(12,2) not null,
-   s_comment   varchar(101)  not null,
-   constraint supplier_pk primary key (s_suppkey)
+   s_comment   varchar(101)  not null
 )
-NOLOGGING INMEMORY MEMCOMPRESS FOR QUERY;
+constraint supplier_pk primary key (s_suppkey);
 
 create table partsupp (
    ps_partkey    integer       not null,
    ps_suppkey    integer       not null,
    ps_availqty   integer       not null,
    ps_supplycost decimal(12,2) not null,
-   ps_comment    varchar(199)  not null,
-   constraint partsupp_pk primary key (ps_partkey,ps_suppkey)
+   ps_comment    varchar(199)  not null
 )
-NOLOGGING INMEMORY MEMCOMPRESS FOR QUERY;
+constraint partsupp_pk primary key (ps_partkey,ps_suppkey);
 
 create table customer (
    c_custkey    integer       not null,
@@ -42,10 +39,9 @@ create table customer (
    c_phone      char(15)      not null,
    c_acctbal    decimal(12,2) not null,
    c_mktsegment char(10)      not null,
-   c_comment    varchar(117)  not null,
-   constraint customer_pk primary key (c_custkey)
+   c_comment    varchar(117)  not null
 )
-NOLOGGING INMEMORY MEMCOMPRESS FOR QUERY;
+constraint customer_pk primary key (c_custkey);
 
 create table orders (
    o_orderkey      integer       not null,
@@ -56,10 +52,9 @@ create table orders (
    o_orderpriority char(15)      not null,
    o_clerk         char(15)      not null,
    o_shippriority  integer       not null,
-   o_comment       varchar(79)   not null,
-   constraint orders_pk primary key (o_orderkey)
+   o_comment       varchar(79)   not null
 )
-NOLOGGING INMEMORY MEMCOMPRESS FOR QUERY;
+constraint orders_pk primary key (o_orderkey);
 
 create table lineitem (
    l_orderkey      integer       not null,
@@ -77,28 +72,21 @@ create table lineitem (
    l_receiptdate   date          not null,
    l_shipinstruct  char(25)      not null,
    l_shipmode      char(10)      not null,
-   l_comment       varchar(44)   not null,
-   constraint lineitem_pk primary key (l_orderkey,l_linenumber)
+   l_comment       varchar(44)   not null
 )
-NOLOGGING INMEMORY MEMCOMPRESS FOR QUERY;
+constraint lineitem_pk primary key (l_orderkey,l_linenumber);
 
 create table nation (
    n_nationkey integer      not null,
    n_name      char(25)     not null,
    n_regionkey integer      not null,
-   n_comment   varchar(152) not null,
-   constraint nation_pk primary key (n_nationkey)
+   n_comment   varchar(152) not null
 )
-NOLOGGING INMEMORY MEMCOMPRESS FOR QUERY;
+constraint nation_pk primary key (n_nationkey);
 
 create table region (
    r_regionkey integer      not null,
    r_name      char(25)     not null,
-   r_comment   varchar(152) not null,
-   constraint region_pk primary key (r_regionkey)
+   r_comment   varchar(152) not null
 )
-NOLOGGING INMEMORY MEMCOMPRESS FOR QUERY;
-
-/
-
-exit
+constraint region_pk primary key (r_regionkey);
