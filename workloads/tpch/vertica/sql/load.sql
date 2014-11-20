af49a4bdd8c7aee3cd83548162d3cf2724df8691
Yanif Ahmad <yanif@jhu.edu>
2014-11-19 21:19:06 -0500

    Added vertica load script

---
+++ workloads/tpch/vertica/sql/load.sql
@@ -0,0 +1,8 @@
+copy part     from '@@PATH@@/@@PART@@'     delimiter '|';
+copy supplier from '@@PATH@@/@@SUPPLIER@@' delimiter '|';
+copy partsupp from '@@PATH@@/@@PARTSUPP@@' delimiter '|';
+copy customer from '@@PATH@@/@@CUSTOMER@@' delimiter '|';
+copy orders   from '@@PATH@@/@@ORDERS@@'   delimiter '|';
+copy lineitem from '@@PATH@@/@@LINEITEM@@' delimiter '|';
+copy nation   from '@@PATH@@/@@NATION@@'   delimiter '|';
+copy region   from '@@PATH@@/@@REGION@@'   delimiter '|';
