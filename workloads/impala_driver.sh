ce483e68fda9349d072c1c7026da99162d208eb1
root <root@qp-hm1.damsl.cs.jhu.edu>
2014-12-10 19:01:50 +0000

    Impala driver: ability to switch scale factors

--- workloads/impala_driver.sh
+++ workloads/impala_driver.sh
@@ -3,7 +3,9 @@
 # Point this script at a workload directory
 # with 'queries' and 'schema' sub-directories
 
-# Also provide the scale factor and number of trials
+# Also provide the scale factor (For TPCH) and number of trials
+# For TPCH, valid scale factors are: 10g, 100g, 500g, 1t
+# For amplab, we only have 1 SF: sf5
 
 if [ $# -ne 3 ] 
 then
@@ -17,13 +19,19 @@ SF=$2
 mkdir impala_results
 rm impala_results/*
 
+# Create a database for this SF
+impala-shell -i qp-hm1.damsl.cs.jhu.edu -q "CREATE DATABASE $SF"
+
 # Load the schema
 SCHEMA_DIR=$1/schema
 for f in $(ls $SCHEMA_DIR);
 do 
   echo "Creating $SCHEMA_DIR/$f"
-  sed s/@@SCALE_FACTOR@@/$SF/g $SCHEMA_DIR/$f | impala-shell -i qp-hm1.damsl.cs.jhu.edu
+  sed s/@@SCALE_FACTOR@@/$SF/g $SCHEMA_DIR/$f > /tmp/query.sql
+  impala-shell -d $SF -i qp-hm1.damsl.cs.jhu.edu -f /tmp/query.sql
 done
+rm /tmp/query.sql
+
 
 # Run each query multiple trials
 QUERY_DIR=$1/queries
@@ -33,6 +41,6 @@ do
   for i in $(seq 1 $3);
   do
     echo "  Trial $i"
-    impala-shell -i qp-hm1.damsl.cs.jhu.edu -f $QUERY_DIR/$q 2>&1 >/dev/null | grep 'Fetched' >> impala_results/$q\_result;
+    impala-shell -d $SF -i qp-hm1.damsl.cs.jhu.edu -f $QUERY_DIR/$q 2>&1 >/dev/null | grep 'Fetched' >> impala_results/$q\_$SF\_result;
   done;
 done
