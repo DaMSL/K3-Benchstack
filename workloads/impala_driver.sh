908a6f52568fdce0b2ec2451dac3179f7ae641df
root <root@qp-hm1.damsl.cs.jhu.edu>
2014-12-16 18:26:40 +0000

    minor changes to impala workload for q11

--- workloads/impala_driver.sh
+++ workloads/impala_driver.sh
@@ -7,13 +7,13 @@
 # For TPCH, valid scale factors are: 10g, 100g, 500g, 1t
 # For amplab, we only have 1 SF: sf5
 
-if [ $# -ne 3 ] 
+if [ $# -ne 4 ] 
 then
-  echo "Usage: $0 workload_folder scale_factor num_trials"
+  echo "Usage: $0  queries_folder schema_folder scale_factor num_trials"
   exit 1
 fi
 
-SF=$2
+SF=$3
 
 # Create a folder for the results
 mkdir impala_results
@@ -23,7 +23,7 @@ rm impala_results/*
 impala-shell -i qp-hm1.damsl.cs.jhu.edu -q "CREATE DATABASE $SF"
 
 # Load the schema
-SCHEMA_DIR=$1/schema
+SCHEMA_DIR=$2
 for f in $(ls $SCHEMA_DIR);
 do 
   echo "Creating $SCHEMA_DIR/$f"
@@ -34,11 +34,11 @@ rm /tmp/query.sql
 
 
 # Run each query multiple trials
-QUERY_DIR=$1/queries
+QUERY_DIR=$1
 for q in $(ls $QUERY_DIR);
 do
   echo "Running $QUERY_DIR/$q"
-  for i in $(seq 1 $3);
+  for i in $(seq 1 $4);
   do
     echo "  Trial $i"
     impala-shell -d $SF -i qp-hm1.damsl.cs.jhu.edu -f $QUERY_DIR/$q 2>&1 >/dev/null | grep 'Fetched' >> impala_results/$q\_$SF\_result;
