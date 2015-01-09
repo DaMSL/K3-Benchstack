155a544bc9a46477e319e9bf16c34f291d3d4890
root <root@damsl.damsl.cs.jhu.edu>
2015-01-09 19:59:07 +0000

    oracle stats

--- launcher/src/systems/oracle/sql/getlast.sql
+++ launcher/src/systems/oracle/sql/getlast.sql
@@ -1,46 +1,37 @@
 set long 20000;
 set longc 200000;
+set lin 1000;
 set colsep ',';
-set lin 2000;
-set pages 0;
 set term on;
 set serveroutput on;
-VARIABLE lastsql VARCHAR2(16);
-BEGIN
-  SELECT /* @@IGNORE_FLAG@@ */ sql_id 
-    INTO :lastsql
-    FROM v$sql 
-    WHERE sql_fulltext like '%@@QUERYFLAG@@%' and sql_fulltext NOT LIKE '%@@IGNORE_FLAG@@%' and rownum=1;
-END;
-/
-SELECT extract(second from max(sample_time) - min(sample_time))
+SELECT extract(second from max(sample_time) - min(sample_time)) as Total_Run_Time
 FROM v$active_session_history
-WHERE sql_id=:lastsql;
+WHERE sql_id='&1';
+SELECT extract(second from max(sample_time) - min(sample_time)) as Actual_Execute_time
+FROM v$active_session_history
+WHERE sql_id='&1' AND sql_exec_id is not null;
 SELECT qstats.*
 ,numsamples * 100 AS Time_ms
 ,round((numsamples / totalsamples * 100), 2) AS PERCENT
 FROM (
 SELECT plan_line_id
   ,plan_operation || ' ' || plan_options
-  ,plan_object_name
+  ,count(distinct session_id) as parallel_ops
   ,count(ash.sql_plan_line_id) AS numsamples
-  ,coalesce(max(workarea_max_mem), 0)/1024/1024
-FROM v$sql_plan_monitor pm
+  ,mem
+FROM (
+select sql_id, plan_line_id, plan_operation, plan_options, max(workarea_max_mem) as mem from v$sql_plan_monitor where sql_id='br03p3uvm8jzy' group by sql_id, plan_line_id, plan_operation, plan_options  order by plan_line_id) pm
   ,v$active_session_history ash
 WHERE pm.plan_line_id = ash.sql_plan_line_id(+)
   AND pm.sql_id = ash.sql_id(+)
-  AND pm.sql_exec_id = ash.sql_exec_id(+)
-  AND pm.sql_id = :lastsql
-  AND pm.sid IN (
-    SELECT sid FROM v$session)
+--  AND pm.sql_exec_id = ash.sql_exec_id(+)
+  AND pm.sql_id='&1'
 GROUP BY plan_line_id
   ,plan_operation || ' ' || plan_options
-  ,plan_object_name
+  , mem
 ORDER BY plan_line_id
 ) qstats
 ,(
   SELECT count(*) AS totalsamples
   FROM v$active_session_history
-  WHERE sql_id = :lastsql
-    AND sql_exec_id IS NOT NULL
-  ) ns;
+  WHERE sql_id='&1' and sql_exec_id is not null) ns;
