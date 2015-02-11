57e6b1e5eaf3c11102277a7fdf5c064de3ee3bbc
root <root@qp1.damsl.cs.jhu.edu>
2015-02-11 20:23:28 +0000

    cadvisor readme

--- launcher/env.sh
+++ launcher/env.sh
@@ -15,7 +15,7 @@ export IMPALA_HOST=qp-hm1.damsl.cs.jhu.edu
 # Oracle
 # For now, we use mddb for tpch100g, and mddb2 for tpch10g
 # TODO use mddb only. with a different database for tpch10g and tpch100g.
-#export ORACLE_HOST=mddb2
+export ORACLE_HOST=mddb2
 export ORACLE_PORT=11521
 
 # Spark
