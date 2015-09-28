a7863027d5a0615da3964fe3afccb6664022fb2a
Yanif Ahmad <yanif@jhu.edu>
2015-09-28 16:53:32 -0400

    Updated flink to use v0.9.1

--- engines/flink/docker/jobmanager.sh
+++ engines/flink/docker/jobmanager.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ################################################################################
 #  Licensed to the Apache Software Foundation (ASF) under one
 #  or more contributor license agreements.  See the NOTICE file
@@ -20,40 +20,27 @@
 
 STARTSTOP=$1
 EXECUTIONMODE=$2
+STREAMINGMODE=$3
 
 bin=`dirname "$0"`
 bin=`cd "$bin"; pwd`
 
 . "$bin"/config.sh
 
-if [ "$EXECUTIONMODE" = "local" ]; then
-    FLINK_JM_HEAP=`expr $FLINK_JM_HEAP + $FLINK_TM_HEAP`
-fi
+JAVA_VERSION=$($JAVA_RUN -version 2>&1 | sed 's/.*version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
 
-JVM_ARGS="$JVM_ARGS -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m"
-
-if [ "$FLINK_JM_HEAP" -gt 0 ]; then
-    JVM_ARGS="$JVM_ARGS -Xms"$FLINK_JM_HEAP"m -Xmx"$FLINK_JM_HEAP"m"
+# Only set JVM 8 arguments if we have correctly extracted the version
+if [[ ${JAVA_VERSION} =~ ${IS_NUMBER} ]]; then
+    if [ "$JAVA_VERSION" -lt 18 ]; then
+        JVM_ARGS="$JVM_ARGS -XX:MaxPermSize=256m"
+    fi
 fi
 
 if [ "$FLINK_IDENT_STRING" = "" ]; then
     FLINK_IDENT_STRING="$USER"
 fi
 
-# auxilliary function to construct a the classpath for the jobmanager
-constructJobManagerClassPath() {
-    for jarfile in "$FLINK_LIB_DIR"/*.jar ; do
-        if [[ $FLINK_JM_CLASSPATH = "" ]]; then
-            FLINK_JM_CLASSPATH=$jarfile;
-        else
-            FLINK_JM_CLASSPATH=$FLINK_JM_CLASSPATH:$jarfile
-        fi
-    done
-
-    echo $FLINK_JM_CLASSPATH
-}
-
-FLINK_JM_CLASSPATH=`manglePathList "$(constructJobManagerClassPath)"`
+FLINK_JM_CLASSPATH=`constructFlinkClassPath`
 
 log=$FLINK_LOG_DIR/flink-$FLINK_IDENT_STRING-jobmanager-$HOSTNAME.log
 out=$FLINK_LOG_DIR/flink-$FLINK_IDENT_STRING-jobmanager-$HOSTNAME.out
@@ -63,6 +50,38 @@ log_setting=(-Dlog.file="$log" -Dlog4j.configuration=file:"$FLINK_CONF_DIR"/log4
 case $STARTSTOP in
 
     (start)
+
+        if [ -z $EXECUTIONMODE ]; then
+            echo "Please specify 'start (cluster|local) [batch|streaming]' or 'stop'"
+            exit 1
+        fi
+
+        # Use batch mode as default
+        if [ -z $STREAMINGMODE ]; then
+            echo "Did not specify [batch|streaming] mode. Falling back to batch mode as default."
+            STREAMINGMODE="batch"
+        fi
+
+        if [[ ! ${FLINK_JM_HEAP} =~ $IS_NUMBER ]]; then
+            echo "ERROR: Configured job manager heap size is not a number. Cancelling job manager startup."
+
+            exit 1
+        fi
+
+        if [ "$EXECUTIONMODE" = "local" ]; then
+            if [[ ! ${FLINK_TM_HEAP} =~ $IS_NUMBER ]]; then
+                echo "ERROR: Configured task manager heap size is not a number. Cancelling (local) job manager startup."
+
+                exit 1
+            fi
+
+            FLINK_JM_HEAP=`expr $FLINK_JM_HEAP + $FLINK_TM_HEAP`
+        fi
+
+        if [ "$FLINK_JM_HEAP" -gt 0 ]; then
+            JVM_ARGS="$JVM_ARGS -Xms"$FLINK_JM_HEAP"m -Xmx"$FLINK_JM_HEAP"m"
+        fi
+
         mkdir -p "$FLINK_PID_DIR"
         if [ -f $pid ]; then
             if kill -0 `cat $pid` > /dev/null 2>&1; then
@@ -75,27 +94,27 @@ case $STARTSTOP in
         rotateLogFile $log
         rotateLogFile $out
 
-        echo Starting job manager
-        $JAVA_RUN $JVM_ARGS ${FLINK_ENV_JAVA_OPTS} "${log_setting[@]}" -classpath "$FLINK_JM_CLASSPATH" org.apache.flink.runtime.jobmanager.JobManager -executionMode $EXECUTIONMODE -configDir "$FLINK_CONF_DIR"  > "$out" 2>&1 < /dev/null &
+        echo "Starting Job Manager"
+        $JAVA_RUN $JVM_ARGS ${FLINK_ENV_JAVA_OPTS} "${log_setting[@]}" -classpath "`manglePathList "$FLINK_JM_CLASSPATH:$INTERNAL_HADOOP_CLASSPATHS"`" org.apache.flink.runtime.jobmanager.JobManager --configDir "$FLINK_CONF_DIR" --executionMode $EXECUTIONMODE --streamingMode "$STREAMINGMODE" > "$out" 2>&1 < /dev/null &
         echo $! > $pid
-        wait $(cat $pid)
+
     ;;
 
     (stop)
         if [ -f $pid ]; then
             if kill -0 `cat $pid` > /dev/null 2>&1; then
-                echo Stopping job manager
+                echo "Stopping job manager"
                 kill `cat $pid`
             else
-                echo No job manager to stop
+                echo "No job manager to stop"
             fi
         else
-            echo No job manager to stop
+            echo "No job manager to stop"
         fi
     ;;
 
     (*)
-        echo Please specify start or stop
+        echo "Please specify 'start (cluster|local) [batch|streaming]' or 'stop'"
     ;;
 
 esac
