3ace1d4e0cd3f4687af4cecc7cb054802b6ae5a9
Yanif Ahmad <yanif@jhu.edu>
2015-09-30 19:46:24 -0400

    Finished up Q1 w/ correct results on 10G

--- workloads/flink/src/main/scala/edu/jhu/cs/damsl/k3/tpch/TPCHQuery1.scala
+++ workloads/flink/src/main/scala/edu/jhu/cs/damsl/k3/tpch/TPCHQuery1.scala
@@ -4,13 +4,14 @@ import org.apache.flink.api.scala._
 import org.apache.flink.api.common.functions._
 import org.apache.flink.util._
 import scala.collection.JavaConverters._
+import org.apache.flink.core.fs.FileSystem.WriteMode
 
 object TPCHQuery1 {
   def main(args: Array[String]) {
     if (!parseParameters(args)) {
       return
     }
-    
+
     // set filter date
     val dateFormat = new java.text.SimpleDateFormat("yyyy-MM-dd")
     val date = dateFormat.parse("1998-09-02")
@@ -23,22 +24,25 @@ object TPCHQuery1 {
         l => dateFormat.parse(l.l_shipdate).before(date)
               || dateFormat.parse(l.l_shipdate).before(date) )
 
-    // group by order and aggregate revenue 
-    val result : DataSet[Q1Acc] = lineitems.groupBy("returnflag", "linestatus")
-                                           .reduceGroup(new Q1GroupReduceFunction())
+    // group by order and aggregate revenue
+    val result : DataSet[Q1Result] = lineitems.groupBy("l_returnflag", "l_linestatus")
+                                              .reduceGroup(new Q1GroupReduceFunction())
 
-        // emit result
-    result.writeAsCsv(outputPath, "\n", "|")
-    
+    // emit result at every taskmanager
+    result.writeAsText(outputPath, WriteMode.OVERWRITE)
+
     // execute program
     env.execute("Scala TPCH Q1")
-
   }
 
-  class Q1GroupReduceFunction extends GroupReduceFunction[Lineitem,Q1Acc] {
-    override def reduce(in: java.lang.Iterable[Lineitem], out: Collector[Q1Acc]) : Unit = {
-      var acc : Q1Acc = new Q1Acc()
+  class Q1GroupReduceFunction extends GroupReduceFunction[Lineitem,Q1Result] {
+    override def reduce(in: java.lang.Iterable[Lineitem], out: Collector[Q1Result]) : Unit = {
+      var acc : Q1Result = new Q1Result()
       for (r <- in.asScala) {
+        if ( acc.l_returnflag.isEmpty() || acc.l_linestatus.isEmpty() ) {
+          acc.l_returnflag = r.l_returnflag
+          acc.l_linestatus = r.l_linestatus
+        }
         acc.sum_qty        += r.l_quantity
         acc.sum_base_price += r.l_extendedprice
         acc.sum_disc_price += (r.l_extendedprice * (1 - r.l_discount))
@@ -51,39 +55,45 @@ object TPCHQuery1 {
       acc.avg_qty   = acc.avg_qty   / acc.count_order
       acc.avg_price = acc.avg_price / acc.count_order
       acc.avg_disc  = acc.avg_disc  / acc.count_order
-      out.collect(acc) 
+      out.collect(acc)
     }
   }
-  
+
   // *************************************************************************
   //     USER DATA TYPES
   // *************************************************************************
-  
+
   case class Lineitem(l_quantity      : Double,
                       l_extendedprice : Double,
                       l_discount      : Double,
                       l_tax           : Double,
                       l_returnflag    : String,
-                      l_returnstatus  : String,
+                      l_linestatus    : String,
                       l_shipdate      : String )
-                      
-  class Q1Acc(var sum_qty         : Double,
-              var sum_base_price  : Double,
-              var sum_disc_price  : Double,
-              var sum_charge      : Double,
-              var avg_qty         : Double,
-              var avg_price       : Double,
-              var avg_disc        : Double,
-              var count_order     : Int)
+
+  class Q1Result(var l_returnflag    : String,
+                 var l_linestatus    : String,
+                 var sum_qty         : Double,
+                 var sum_base_price  : Double,
+                 var sum_disc_price  : Double,
+                 var sum_charge      : Double,
+                 var avg_qty         : Double,
+                 var avg_price       : Double,
+                 var avg_disc        : Double,
+                 var count_order     : Int)
   {
-    def this() = this(0.0,0.0,0.0,0.0,0.0,0.0,0.0,0)
+    def this() = this("", "", 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0)
+
+    override def toString() = {
+      l_returnflag + "|" + l_linestatus + "|" + sum_qty + "|" + sum_base_price + "|" + sum_disc_price + "|" + sum_charge + "|" + avg_qty + "|" + avg_price + "|" + avg_disc + "|" + count_order
+    }
   }
 
 
   // *************************************************************************
   //     UTIL METHODS
   // *************************************************************************
-  
+
   private var lineitemPath: String = null
   private var outputPath: String = null
 
@@ -97,7 +107,7 @@ object TPCHQuery1 {
       false
     }
   }
-  
+
   private def getLineitemDataSet(env: ExecutionEnvironment): DataSet[Lineitem] = {
     env.readCsvFile[Lineitem](
         lineitemPath,
